487eebdcf0da4eb93d127f1c1518b15eaf1aaad1
paulhwang <phwang@gmail.com>
2021-02-19 22:16:55 +0800

    encode

--- src/Phwang/Utils/Encode/EncodeNumberClass.java
+++ src/Phwang/Utils/Encode/EncodeNumberClass.java
@@ -11,28 +11,25 @@ package Phwang.Utils.Encode;
 public class EncodeNumberClass {
     public static String EncodeNumber(int number_val, int size_val)
     {
-        //String str = number_val.ToString();
+        String str = Integer.toString(number_val);
 
         var buf = "";
-        /*
-        for (var i = str.Length; i < size_val; i++)
+        for (var i = str.length(); i < size_val; i++)
         {
             buf = buf + "0";
         }
         buf = buf + str;
-        */
         return buf;
     }
 
-    /*
     public static int DecodeNumber(String str_val)
     {
-        int str_len = str_val.Length;
+        int str_len = str_val.length();
         int val = 0;
 
         for (int i = 0; i < str_len; i++)
         {
-            val += str_val[i] - '0';
+            val += str_val.charAt(i) - '0';
             if (i < str_len - 1)
             {
                 val *= 10;
@@ -41,5 +38,4 @@ public class EncodeNumberClass {
 
         return val;
     }
-    */
 }
