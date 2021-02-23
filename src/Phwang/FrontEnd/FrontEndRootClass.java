f26fadd18a77340cfc7b542cabec328b659af5a2
paul.hwang <phwang@gmail.com>
2021-02-23 14:54:01 +0800

    test

--- src/Phwang/FrontEnd/FrontEndRootClass.java
+++ src/Phwang/FrontEnd/FrontEndRootClass.java
@@ -19,7 +19,8 @@ public class FrontEndRootClass {
     private FrontTestClass theFrontTestObject;
     
     public ThreadMgrClass ThreadMgrObject() { return this.threadMgrObject; }
-    private UFrontClass UFrontObject() { return this.uFrontObject; }
+    public UFrontClass UFrontObject() { return this.uFrontObject; }
+    private FrontTestClass FrontTestObject() { return theFrontTestObject; }
 
     public FrontEndRootClass() {
         this.debugIt(false, "FrontEndRootClass", "init start");
@@ -29,6 +30,7 @@ public class FrontEndRootClass {
         this.theFrontTestObject = new FrontTestClass(this);
         
         this.UFrontObject().startThreads();
+        this.FrontTestObject().StartTest();
     }
 
     private void debugIt(Boolean on_off_val, String str0_val, String str1_val) { if (on_off_val) this.logitIt(str0_val, str1_val); }
--- src/Phwang/FrontEnd/FrontTestClass.java
+++ src/Phwang/FrontEnd/FrontTestClass.java
@@ -8,12 +8,12 @@
 
 package Phwang.FrontEnd;
 
-import Phwang.Protocols.FabricFrontEndProtocolClass;
 import Phwang.Utils.AbendClass;
-import Phwang.Utils.Binder.BinderClass;
+import Phwang.Utils.UtilsClass;
+import Phwang.Utils.ThreadMgr.ThreadInterface;
 import Phwang.Utils.ThreadMgr.ThreadMgrClass;
 
-public class FrontTestClass {
+public class FrontTestClass implements ThreadInterface {
     private String objectName() {return "FrontTestClass";}
     private String FrontTestThreadName() { return "FrontTestThread"; }
 
@@ -21,13 +21,32 @@ public class FrontTestClass {
     
     public FrontEndRootClass FrontEndRootObject() { return this.frontEndRootObject; }
     private ThreadMgrClass ThreadMgrObject() { return this.FrontEndRootObject().ThreadMgrObject();}
+    private UFrontClass UFrontObject() { return this.FrontEndRootObject().UFrontObject();}
 
     public FrontTestClass(FrontEndRootClass root_object_val) {
         this.debugIt(false, "FrontTestClass", "init start");
         
         this.frontEndRootObject = root_object_val;
     }
-
+    
+    public void StartTest() {
+    	this.ThreadMgrObject().CreateThreadObject(this.FrontTestThreadName(), this);
+     }
+    
+	public void ThreadCallbackFunction() {
+		this.UFrontReceiveThreadFunc();
+	}
+    
+    private void UFrontReceiveThreadFunc() {
+        this.debugIt(false, "UFrontReceiveThreadFunc", "start " + this.FrontTestThreadName());
+    	UtilsClass.sleep(2000);
+    	
+    	this.DoTest();
+    }
+    
+    private void DoTest() {
+    	//String ajex_response_str = this.UFrontObject().ProcessAjaxRequestPacket("aaa");
+    }
 
     private void debugIt(Boolean on_off_val, String str0_val, String str1_val) { if (on_off_val) this.logitIt(str0_val, str1_val); }
     private void logitIt(String str0_val, String str1_val) { AbendClass.phwangLogit(this.objectName() + "." + str0_val + "()", str1_val); }
