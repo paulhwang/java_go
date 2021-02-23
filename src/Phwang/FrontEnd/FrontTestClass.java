f56ec2257ba1617942302ac9ba86809307ca4d45
paul.hwang <phwang@gmail.com>
2021-02-23 14:26:49 +0800

    front test

--- src/Phwang/FrontEnd/FrontTestClass.java
+++ src/Phwang/FrontEnd/FrontTestClass.java
@@ -8,28 +8,26 @@
 
 package Phwang.FrontEnd;
 
+import Phwang.Protocols.FabricFrontEndProtocolClass;
 import Phwang.Utils.AbendClass;
+import Phwang.Utils.Binder.BinderClass;
 import Phwang.Utils.ThreadMgr.ThreadMgrClass;
 
-public class FrontEndRootClass {
-    private String objectName() {return "FrontEndRootClass";}
-    
-    private ThreadMgrClass threadMgrObject;
-    private UFrontClass uFrontObject;
-    private FrontTestClass theFrontTestObject;
-    
-    public ThreadMgrClass ThreadMgrObject() { return this.threadMgrObject; }
-    private UFrontClass UFrontObject() { return this.uFrontObject; }
+public class FrontTestClass {
+    private String objectName() {return "FrontTestClass";}
+    private String FrontTestThreadName() { return "FrontTestThread"; }
 
-    public FrontEndRootClass() {
-        this.debugIt(false, "FrontEndRootClass", "init start");
+    private FrontEndRootClass frontEndRootObject;
+    
+    public FrontEndRootClass FrontEndRootObject() { return this.frontEndRootObject; }
+    private ThreadMgrClass ThreadMgrObject() { return this.FrontEndRootObject().ThreadMgrObject();}
+
+    public FrontTestClass(FrontEndRootClass root_object_val) {
+        this.debugIt(false, "FrontTestClass", "init start");
         
-        this.threadMgrObject = new ThreadMgrClass();
-        this.uFrontObject = new UFrontClass(this);
-        this.theFrontTestObject = new FrontTestClass(this);
-        
-        this.UFrontObject().startThreads();
+        this.frontEndRootObject = root_object_val;
     }
+
 
     private void debugIt(Boolean on_off_val, String str0_val, String str1_val) { if (on_off_val) this.logitIt(str0_val, str1_val); }
     private void logitIt(String str0_val, String str1_val) { AbendClass.phwangLogit(this.objectName() + "." + str0_val + "()", str1_val); }
