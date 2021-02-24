ac00813f2d6a010dcdecda75f27a08f9a3c05bd7
paulhwang <phwang@gmail.com>
2021-02-24 19:20:37 +0800

    testcase

---
+++ src/Phwang/FrontEnd/FrontTestCaseClass.java
@@ -0,0 +1,79 @@
+/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2018 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+package Phwang.FrontEnd;
+
+import org.json.simple.JSONObject;
+import Phwang.Utils.AbendClass;
+import Phwang.Utils.Encode.EncodeNumberClass;
+import Phwang.Utils.ThreadMgr.ThreadInterface;
+import Phwang.Utils.ThreadMgr.ThreadMgrClass;
+
+/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2018 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+class FrontTestCaseClass implements ThreadInterface {
+    private String objectName() {return "FrontTestClass";}
+    private String frontTestCaseThreadName() { return "FrontTestCaseThread"; }
+    
+    private FrontTestClass frontTestObject;
+    private String indexString;
+    
+    public FrontEndRootClass FrontEndRootObject() { return this.frontTestObject.FrontEndRootObject(); }
+    private ThreadMgrClass ThreadMgrObject() { return this.FrontEndRootObject().ThreadMgrObject();}
+    private UFrontClass UFrontObject() { return this.FrontEndRootObject().UFrontObject();}
+
+    public FrontTestCaseClass(FrontTestClass FrontTestClass, int index_val) {
+        this.debugIt(false, "FrontTestClass", "init start");
+        
+        this.frontTestObject = FrontTestClass;
+        this.indexString = EncodeNumberClass.EncodeNumber(index_val, 3);
+    }
+    
+    public void startTestTest() {
+    	this.ThreadMgrObject().CreateThreadObject(this.frontTestCaseThreadName(), this);
+     }
+
+	public void ThreadCallbackFunction() {
+		this.frontTestCaseThreadFunc();
+	}
+    
+    private void frontTestCaseThreadFunc() {
+        try {
+        	Thread.sleep(100);
+        }
+        catch (Exception ignore) {}
+    	
+    	this.doTest();
+    }
+    
+    private void doTest() {
+    	JSONObject json_data = new JSONObject();
+    	json_data.put("my_name", "Test_" + this.indexString);
+    	json_data.put("password", "tennis");
+    	String json_str_data = json_data.toJSONString();
+    	
+    	JSONObject json = new JSONObject();
+    	json.put("command", "setup_link");
+    	json.put("data", json_str_data);
+    	String json_str = json.toJSONString();
+    	
+    	String ajex_response_str = this.UFrontObject().ProcessAjaxRequestPacket(json_str);
+        this.debugIt(true, "DoTest", "ajex_response data=" + ajex_response_str);
+    }
+
+    private void debugIt(Boolean on_off_val, String str0_val, String str1_val) { if (on_off_val) this.logitIt(str0_val, str1_val); }
+    private void logitIt(String str0_val, String str1_val) { AbendClass.phwangLogit(this.objectName() + "." + str0_val + "()", str1_val); }
+    public void abendIt(String str0_val, String str1_val) { AbendClass.phwangAbend(this.objectName() + "." + str0_val + "()", str1_val); }
+}
+
--- src/Phwang/FrontEnd/FrontTestClass.java
+++ src/Phwang/FrontEnd/FrontTestClass.java
@@ -46,7 +46,7 @@ public class FrontTestClass implements ThreadInterface {
         }
         catch (Exception ignore) {}
     	
-        for (int j = 0; j < 100; j++) {
+        for (int j = 0; j <10; j++) {
         	for (int i = 0; i < 5; i++) {
         		FrontTestCaseClass test_case = new FrontTestCaseClass(this, i);
         		test_case.startTestTest();
@@ -63,59 +63,3 @@ public class FrontTestClass implements ThreadInterface {
     private void logitIt(String str0_val, String str1_val) { AbendClass.phwangLogit(this.objectName() + "." + str0_val + "()", str1_val); }
     public void abendIt(String str0_val, String str1_val) { AbendClass.phwangAbend(this.objectName() + "." + str0_val + "()", str1_val); }
 }
-
-class FrontTestCaseClass implements ThreadInterface {
-    private String objectName() {return "FrontTestClass";}
-    private String frontTestCaseThreadName() { return "FrontTestCaseThread"; }
-    
-    private FrontTestClass frontTestObject;
-    private String indexString;
-    
-    public FrontEndRootClass FrontEndRootObject() { return this.frontTestObject.FrontEndRootObject(); }
-    private ThreadMgrClass ThreadMgrObject() { return this.FrontEndRootObject().ThreadMgrObject();}
-    private UFrontClass UFrontObject() { return this.FrontEndRootObject().UFrontObject();}
-
-    public FrontTestCaseClass(FrontTestClass FrontTestClass, int index_val) {
-        this.debugIt(false, "FrontTestClass", "init start");
-        
-        this.frontTestObject = FrontTestClass;
-        this.indexString = EncodeNumberClass.EncodeNumber(index_val, 3);
-    }
-    
-    public void startTestTest() {
-    	this.ThreadMgrObject().CreateThreadObject(this.frontTestCaseThreadName(), this);
-     }
-
-	public void ThreadCallbackFunction() {
-		this.frontTestCaseThreadFunc();
-	}
-    
-    private void frontTestCaseThreadFunc() {
-        try {
-        	Thread.sleep(100);
-        }
-        catch (Exception ignore) {}
-    	
-    	this.doTest();
-    }
-    
-    private void doTest() {
-    	JSONObject json_data = new JSONObject();
-    	json_data.put("my_name", "Test_" + this.indexString);
-    	json_data.put("password", "tennis");
-    	String json_str_data = json_data.toJSONString();
-    	
-    	JSONObject json = new JSONObject();
-    	json.put("command", "setup_link");
-    	json.put("data", json_str_data);
-    	String json_str = json.toJSONString();
-    	
-    	String ajex_response_str = this.UFrontObject().ProcessAjaxRequestPacket(json_str);
-        this.debugIt(true, "DoTest", "ajex_response data=" + ajex_response_str);
-    }
-
-    private void debugIt(Boolean on_off_val, String str0_val, String str1_val) { if (on_off_val) this.logitIt(str0_val, str1_val); }
-    private void logitIt(String str0_val, String str1_val) { AbendClass.phwangLogit(this.objectName() + "." + str0_val + "()", str1_val); }
-    public void abendIt(String str0_val, String str1_val) { AbendClass.phwangAbend(this.objectName() + "." + str0_val + "()", str1_val); }
-}
-
