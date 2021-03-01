1e7dae80d200884b2fcbca827cfa0df4351eeced
paulhwang <phwang@gmail.com>
2021-03-01 08:50:16 +0800

    abend lock

--- src/phwang/utils/AbendClass.java
+++ src/phwang/utils/AbendClass.java
@@ -1,26 +1,35 @@
 package phwang.utils;
 
+import java.util.concurrent.locks.*;
+
 public class AbendClass {
-	static LockBooleanClass logStopped = new LockBooleanClass();
-	static Boolean crashHere;
+	static boolean logStopped = false;
+    static Lock abendLock = new ReentrantLock();
 	
     static public void log(String s0, String s1) {
-    	if (logStopped.get())
+    	if (logStopped) {
     		return;
+    	}
+    	
         System.out.println(s0 + " " + s1);
     }
 
     static public void abend(String s0, String s1) {
-    	System.out.println("abend+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    	if (logStopped) {
+    		return;
+    	}
+    	logStopped = true;
+   	
+    	abendLock.lock();
+    	System.out.println("***********************ABEND*************************");
     	System.out.println(s0 + " " + s1);
-    	System.out.println("abend+++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    	System.out.println("***********************ABEND*************************");
     	forceCrash();
+    	abendLock.unlock();
     }
     
+	static Boolean crashHere;
     static private void forceCrash() {
-    	logStopped.set(true);
-    	
-    	if (crashHere)
-    		return;
+    	if (crashHere) return;
     }
 }
