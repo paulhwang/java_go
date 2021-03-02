cc1bdbae11b6af56b9199a25a60076fcd9fb4d73
paulhwang <phwang@gmail.com>
2021-03-02 12:22:52 +0800

    session

--- src/phwang/fabric/DFabricParserClass.java
+++ src/phwang/fabric/DFabricParserClass.java
@@ -1,7 +1,7 @@
 /*
  ******************************************************************************
  *                                       
- *  Copyright (c) 2018 phwang. All rights reserved.
+ *  Copyright (c) 2021 phwang. All rights reserved.
  *
  ******************************************************************************
  */
@@ -50,6 +50,15 @@ public class DFabricParserClass {
         
         if (json_str.charAt(0) == FabricDefineClass.FABRIC_COMMAND_SETUP_LINK.charAt(0)) {
             response_data = this.processSetupLinkRequest(json_str.substring(1));
+            if (response_data == null) {
+            	this.abend("parseInputPacket", "response_data is null, command=" + command);
+            }
+            this.dFabricObject.transmitData(job_id_str + response_data);
+        	return;
+        }
+        
+        if (json_str.charAt(0) == FabricDefineClass.FABRIC_COMMAND_SETUP_SESSION.charAt(0)) {
+            response_data = this.processSetupSessionRequest1(json_str.substring(1));
             if (response_data == null) {
             	this.abend("parseInputPacket", "response_data is null, command=" + command);
             }
@@ -114,51 +123,79 @@ public class DFabricParserClass {
         this.dFabricObject.transmitData(job_id_str + response_data);
     }
 
+    private String processSetupSessionRequest1(String input_str_val) {
+        this.debug(false, "processSetupSessionRequest", "input_str_val=" + input_str_val);
+        
+        String link_id_str = input_str_val.substring(FabricDefineClass.FABRIC_LINK_ID_SIZE);
+        String rest_str = input_str_val.substring(FabricDefineClass.FABRIC_LINK_ID_SIZE);
+        
+        int his_name_len = EncodeNumberClass.decodeNumber(rest_str.substring(0, ProtocolDefineClass.DATA_LENGTH_SIZE));
+        rest_str = input_str_val.substring(ProtocolDefineClass.DATA_LENGTH_SIZE);
+        String his_name = rest_str.substring(0, his_name_len);
+    	rest_str = rest_str.substring(his_name_len);
+        
+        int theme_data_len = EncodeNumberClass.decodeNumber(rest_str.substring(0, ProtocolDefineClass.DATA_LENGTH_SIZE));
+        rest_str = input_str_val.substring(ProtocolDefineClass.DATA_LENGTH_SIZE);
+        String theme_data_str = rest_str.substring(0, theme_data_len);
+    	//rest_str = rest_str.substring(theme_data_len);
+    	
+        this.debug(true, "processSetupSessionRequest", "link_id = " + link_id_str);
+        this.debug(true, "processSetupSessionRequest", "his_name = " + his_name);
+        this.debug(true, "processSetupSessionRequest", "theme_data = " + theme_data_str);
+
+        String theme_id_str = theme_data_str.substring(0, BrowserDefine.BROWSER_THEME_ID_SIZE);
+        String theme_data = theme_data_str.substring(BrowserDefine.BROWSER_THEME_ID_SIZE);
+
+        LinkClass link = this.LinkMgrObject().getLinkByIdStr(link_id_str);
+        if (link == null) {
+            return this.errorProcessSetupSession(link_id_str, "*************null link");
+        }
+        
+        SessionClass session = link.mallocSession();
+        session.setBrowserThemeIdStr(theme_id_str);
+        GroupClass group = this.GroupMgrObject().mallocGroup(theme_data);
+        if (group == null) {
+        	this.abend("processSetupSessionRequest", "null group");
+            return this.errorProcessSetupSession(link_id_str, "null group");
+        }
+        group.insertSession(session);
+        session.bindGroup(group);
+        
+        if (his_name.equals(link.myName())) {
+            this.mallocRoom(group, theme_data);
+        }
+        else {
+            LinkClass his_link = this.LinkMgrObject().GetLinkByMyName(his_name);
+            if (his_link == null) {
+                return this.errorProcessSetupSession(link_id_str, "his_link does not exist");
+            }
+            SessionClass his_session = his_link.mallocSession();
+            if (his_session == null) {
+                return this.errorProcessSetupSession(link_id_str, "null his_session");
+            }
+
+            group.insertSession(his_session);
+            his_session.bindGroup(group);
+
+            his_link.setPendingSessionSetup(his_link.linkIdStr() + his_session.SessionIdStr(), theme_data);
+        }
+
+        String response_data = this.generateSetupSessionResponse(link.linkIdStr(), session.SessionIdStr());
+        return response_data;
+    }
+
     private String processSetupLinkRequest(String input_str_val) {
-        this.debug(false, "processSetupLinkRequest", "input_str_val = " + input_str_val);
+        this.debug(false, "processSetupLinkRequest", "input_str_val=" + input_str_val);
         
         int my_name_len = EncodeNumberClass.decodeNumber(input_str_val.substring(0, ProtocolDefineClass.DATA_LENGTH_SIZE));
         String rest_str = input_str_val.substring(ProtocolDefineClass.DATA_LENGTH_SIZE);
-        
         String my_name = rest_str.substring(0, my_name_len);
     	rest_str = rest_str.substring(my_name_len);
     	
         int password_len = EncodeNumberClass.decodeNumber(rest_str.substring(0, ProtocolDefineClass.DATA_LENGTH_SIZE));
         rest_str = rest_str.substring(ProtocolDefineClass.DATA_LENGTH_SIZE);
-        
     	String password = rest_str.substring(0, password_len);
-    	
-        this.debug(false, "processSetupLinkRequest", "my_name = " + my_name);
-        this.debug(false, "processSetupLinkRequest", "password = " + password);
-
-        LinkClass link = this.LinkMgrObject().mallocLink(my_name);
-        if (link == null) {
-        	this.abend("processSetupLinkRequest", "link is null");
-        	return null;
-        }
-        String response_data = this.generateSetupLinkResponse(link.linkIdStr(), link.myName());
-        return response_data;
-    }
-
-    private String processSetupLinkRequest00000(String json_str_val) {
-        this.debug(false, "processSetupLinkRequest", "json_str_val = " + json_str_val);
-    	String my_name;
-    	String password;
-
-        try {
-            JSONParser parser = new JSONParser();
-        	JSONObject json = (JSONObject) parser.parse(json_str_val);
-        	my_name = (String) json.get("my_name");
-        	password = (String) json.get("password");
-        }
-        catch(ParseException pe) {
-        	this.abend("processSetupLinkRequest", "ParseException: " + pe + " json_str=" + json_str_val);
-        	return "ParseException";
-        }
-        catch (Exception e) {
-        	this.abend("processSetupLinkRequest", "Exception: " + e + " json_str=" + json_str_val);
-        	return "Exception";
-        }
+    	//rest_str = rest_str.substring(password_len);
     	
         this.debug(false, "processSetupLinkRequest", "my_name = " + my_name);
         this.debug(false, "processSetupLinkRequest", "password = " + password);
