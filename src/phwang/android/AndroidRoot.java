12e2672cd9b7daf3da91470b6f74311954aad891
paulhwang <phwang@gmail.com>
2021-03-06 11:39:53 +0800

    test android

---
+++ src/phwang/android/AndroidRoot.java
@@ -0,0 +1,28 @@
+/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2021 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+package phwang.android;
+
+import phwang.utils.AbendClass;
+
+public class AndroidRoot {
+    private String objectName() {return "AndroidRoot";}
+
+    public AndroidRoot() {
+        this.debug(false, "AndroidRoot", "init start");
+
+	}
+    
+    private void debug(Boolean on_off, String s0, String s1) { if (on_off) this.log(s0, s1); }
+    private void log(String s0, String s1) { this.logIt(this.objectName() + "." + s0 + "()", s1); }
+    public void abend(String s0, String s1) { this.abendIt(this.objectName() + "." + s0 + "()", s1); }
+
+    private Boolean debug_on = true;
+    public void logIt(String s0, String s1) { if (this.debug_on) AbendClass.log(s0, s1); }
+    public void abendIt(String s0, String s1) { if (this.debug_on) AbendClass.abend(s0, s1); }
+}
