068cf3b3d82feca6712643770d78cbd636b2b0a8
paulhwang <phwang@gmail.com>
2021-03-15 09:41:12 +0800

    root

--- src/com/phwang/core/main/PhwangRoot.java
+++ src/com/phwang/core/main/PhwangRoot.java
@@ -14,6 +14,8 @@ import com.phwang.core.fabric.FabricRoot;
 import com.phwang.core.front.FrontRoot;
 import com.phwang.core.android.AndroidRoot;
 import com.phwang.core.models.ModelRootClass;
+import com.phwang.core.test.AndroidTest;
+import com.phwang.core.test.HttpTest;
 import com.phwang.core.theme.ThemeRoot;
 
 public class PhwangRoot {
@@ -26,12 +28,12 @@ public class PhwangRoot {
     private AndroidRoot androidRoot_;
     private ModelRootClass modelRoot_;
     
-    public FrontRoot frontRoot() { return this.frontRoot_; }
-    public FabricRoot fabricRoo() { return this.fabricRoot_; }
-    public ThemeRoot themeRoot() { return this.themeRoot_; }
-    public EngineRoot engineRoot() {return this.engineRoot_; }
-    public AndroidRoot androidRoot() { return this.androidRoot_; }
-    public ModelRootClass modelRoot() { return this.modelRoot_; }
+    protected FrontRoot frontRoot() { return this.frontRoot_; }
+    protected FabricRoot fabricRoo() { return this.fabricRoot_; }
+    protected ThemeRoot themeRoot() { return this.themeRoot_; }
+    protected EngineRoot engineRoot() {return this.engineRoot_; }
+    protected AndroidRoot androidRoot() { return this.androidRoot_; }
+    protected ModelRootClass modelRoot() { return this.modelRoot_; }
 
     public PhwangRoot() {
         this.debug(false, "PhwangRoot", "init start");
@@ -43,8 +45,18 @@ public class PhwangRoot {
     	this.modelRoot_ = new ModelRootClass();
 	}
     
+    public void startTest(Boolean http_test_on, Boolean android_test_on) {
+		if (http_test_on) {
+			new HttpTest(this.frontRoot_.frontDExport()).startTest();
+		}
+		
+		if (android_test_on) {
+			new AndroidTest(this.androidRoot().androidDExport()).startTest();
+		}
+    }
+    
     private void debug(Boolean on_off, String s0, String s1) { if (on_off) this.log(s0, s1); }
     private void log(String s0, String s1) { Abend.log(this.objectName() + "." + s0 + "()", s1); }
-    public void abend(String s0, String s1) { Abend.abend(this.objectName() + "." + s0 + "()", s1); }
+    protected void abend(String s0, String s1) { Abend.abend(this.objectName() + "." + s0 + "()", s1); }
 }
 
