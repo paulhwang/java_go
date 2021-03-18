af9647f544aeaba34478c10220e00fc86c0aa7d0
paulhwang <phwang@gmail.com>
2021-03-18 23:40:47 +0800

    config

--- src/com/phwang/client/ClientGoConfig.java
+++ src/com/phwang/client/ClientGoConfig.java
@@ -8,6 +8,8 @@
 
 package com.phwang.client;
 
+import com.phwang.core.utils.EncodeNumber;
+
 public class ClientGoConfig {
     private int boardSize_;
     private int handicapPoint_;
@@ -16,5 +18,17 @@ public class ClientGoConfig {
     public int boardSize() { return this.boardSize_; }
     public int handicapPoint() { return this.handicapPoint_; }
     public int komiPoint() { return this.komiPoint_; }
+
+    public void setBoardSize(int val) { this.boardSize_ = val; }
+    public void setHandicapPoint(int val) { this.handicapPoint_ = val; }
+    public void setKomiPoint(int val) { this.komiPoint_ = val; }
+    
+    public String getGoConfigStr() {
+    	StringBuilder buf = new StringBuilder();
+    	buf.append(EncodeNumber.encode(this.boardSize_, 2));
+    	buf.append(EncodeNumber.encode(this.handicapPoint_, 1));
+    	buf.append(EncodeNumber.encode(this.handicapPoint_, 1));
+    	return buf.toString();
+    }
 }
 
