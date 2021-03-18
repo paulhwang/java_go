cd373b02636237211b729181a93e6365ccc6a402
paulhwang <phwang@gmail.com>
2021-03-18 21:14:40 +0800

    test

--- src/com/phwang/test/ClientTester.java
+++ src/com/phwang/test/ClientTester.java
@@ -15,19 +15,16 @@ import com.phwang.client.ClientDExport;
 import com.phwang.client.ClientFabricInfo;
 import com.phwang.client.ClientDImportInt;
 
-class ClientTester implements ThreadEntityInt, ClientDImportInt {
+class ClientTester implements ClientDImportInt {
     private String objectName() {return "ClientTester";}
-    private String clientTestCaseThreadName() { return "ClientTestCaseThread"; }
     
     private ClientTest clientTest_;
     private ClientRoot clientRoot_;
     private String myNameStr_;
     private String password_ = "Tennis";
-    private LockedInteger caseIndex_;
         
     private ClientTest clientTest() { return this.clientTest_; }
-    private ThreadMgr threadMgr() { return this.clientTest().threadMgr();}
-    private ClientDExport clientDExport() { return this.clientRoot_.clientDExport(); }
+   private ClientDExport clientDExport() { return this.clientRoot_.clientDExport(); }
     private ClientFabricInfo clientFabricInfo() { return this.clientRoot_.clientFabricInfo();}
     
     protected ClientTester(ClientTest client_test_val, int tester_index_val) {
@@ -36,51 +33,11 @@ class ClientTester implements ThreadEntityInt, ClientDImportInt {
         this.clientTest_ = client_test_val;
         this.myNameStr_ = "client_" + EncodeNumber.encode(tester_index_val, 5);
 		this.clientRoot_ = new ClientRoot(this);
-        this.caseIndex_ = new LockedInteger(0);
     }
     
     protected void startTest() {
-    	this.threadMgr().createThreadObject(this.clientTestCaseThreadName(), this);
+    	this.doSetupLink(this.myNameStr_);
      }
-
-	public void threadCallbackFunction() {
-		this.clientTest().incrementThreadCount();
-		this.clienterThreadFunc();
-		this.clientTest().decrementThreadCount();
-	}
-    
-    private void clienterThreadFunc() {
-        try {
-        	Thread.sleep(100);
-        }
-        catch (Exception ignore) {}
-        
-    	this.caseIndex_.increment();
-    	int case_index = this.caseIndex_.get();
-    	String my_name = this.myNameStr_ + EncodeNumber.encode(case_index, 5);
-    	this.doSetupLink(my_name);
-    	
-    	//UtilsClass.sleep(100);
-    	this.doGetLinkData();
-    	
-    	//UtilsClass.sleep(100);
-    	this.doGetNameList();
-    	
-    	//UtilsClass.sleep(100);
-    	this.doSetupSession();
-    	
-    	//Utils.sleep(100);
-    	this.doSetupSession3();
-    	
-    	//Utils.sleep(100);
-    	this.doPutSessionData();
-    	
-    	//Utils.sleep(100);
-    	this.doGetSessionData();
-    	
-    	//Utils.sleep(100);
-    	this.doSetupSession2();
-    }
     
     private void doSetupLink(String my_name_val) {
     	this.debug(false, "doSetupLink", "doSetupLink");
