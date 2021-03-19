9b78739e56de7161434a108ea4c1cd66eae8c87a
paulhwang <phwang@gmail.com>
2021-03-19 17:11:08 +0800

    go

--- src/com/phwang/client/ClientGoAct.java
+++ src/com/phwang/client/ClientGoAct.java
@@ -8,11 +8,12 @@
 
 package com.phwang.client;
 
+import com.phwang.core.go.GoExport;
 import com.phwang.core.protocols.ProtocolDefineClass;
 import com.phwang.core.utils.EncodeNumber;
 
 public class ClientGoAct {
-	private char action_ = 'M';
+	private char action_ = GoExport.GO_PROTOCOL_MOVE_COMMAND;
     private int x_ = 5;
     private int y_ = 5;
     private int color_ = 1;
@@ -23,6 +24,7 @@ public class ClientGoAct {
     public int color() { return this.color_; }
     public int index() { return this.index_; }
 
+    public void setAction(char val) { this.action_ = val; }
     public void setX(int val) { this.x_ = val; }
     public void setY(int val) { this.y_ = val; }
     public void setColor(int val) { this.color_ = val; }
@@ -32,7 +34,7 @@ public class ClientGoAct {
     	StringBuilder buf = new StringBuilder();
     	buf.append('G');
 		buf.append(this.action_);
-    	if (this.action_ == 'M') {
+    	if (this.action_ == GoExport.GO_PROTOCOL_MOVE_COMMAND) {
     		buf.append(EncodeNumber.encode(this.x_, 2));
     		buf.append(EncodeNumber.encode(this.y_, 2));
     		buf.append(EncodeNumber.encode(this.color_, 1));
