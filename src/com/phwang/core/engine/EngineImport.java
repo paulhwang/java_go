6e06e34388d9a346fbaa199a5f7b17da77981c75
paulhwang <phwang@gmail.com>
2021-03-20 10:47:12 +0800

    engine

---
+++ src/com/phwang/core/engine/EngineImport.java
@@ -0,0 +1,15 @@
+/*
+ ******************************************************************************
+ *                                       
+ *  Copyright (c) 2021 phwang. All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+package com.phwang.core.engine;
+
+import com.phwang.core.theme.ThemeExport;
+
+public class EngineImport {
+    public static final int THEME_ROOM_ID_SIZE = ThemeExport.THEME_ROOM_ID_SIZE;
+}
